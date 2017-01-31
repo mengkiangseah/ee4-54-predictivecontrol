bc49dd0f4b55d4c39b29df8c08ae707a69c9e7ab
Meng Kiang Seah <mengkiang92@gmail.com>
2017-01-31 23:05:24 +0000

    myRHC.m function tested with test.m and also on Cody. All working.

--- assignment2/myRHC.m
+++ assignment2/myRHC.m
@@ -1,8 +1,13 @@
 function K = myRHC(H,G,m)
+% MYRHC K = myRHC(H,G,m)
 % H and G are the cost function matrices
 % m is the number of control inputs
 % K is the RHC law gain
 
-% your code here
-K = 1;
+    % U_bar_star = -(H\G)x0
+    % But only want u0, so:
+    % u0_star(x0) = -[Im 0]*(H\G)*x0
+    % Alternatively, select first m rows of -(H\G) = L
+    L = -(H\G);
+    K = L(1:m, :);
 end
