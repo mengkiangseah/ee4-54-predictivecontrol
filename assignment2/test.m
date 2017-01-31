ed03a361a56aae2cb76841a3daf17977dbb8336d
Meng Kiang Seah <mengkiang92@gmail.com>
2017-01-31 22:23:38 +0000

    Modified test.m to check for myCostMatrices as well.

--- assignment2/test.m
+++ assignment2/test.m
@@ -1,3 +1,4 @@
+% Test for myPrediction
 A = magic(3);
 B = randn(3, 2);
 N = 3;
@@ -8,5 +9,25 @@ Phi_actual = [A; A^2; A^3];
 zr = zeros(3, 2);
 Gamma_actual = [B, zr, zr; A*B, B, zr; A*A*B, A*B, B];
 
-wrong = sum(sum(round(Gamma, 8) ~= round(Gamma_actual, 8))) + sum(sum(round(Phi, 8) ~= round(Phi_actual, 8)));
-disp(wrong);
+wrong1 = sum(sum(round(Gamma, 8) ~= round(Gamma_actual, 8))) + sum(sum(round(Phi, 8) ~= round(Phi_actual, 8)));
+disp(wrong1);
+
+% Test for myCostMatrices
+Qtild = randn(5, 3);
+Q = Qtild' * Qtild;
+Rtild = randn(6, 2);
+R = Rtild' * Rtild;
+Ptild = randn(4, 3);
+P = Ptild' * Ptild;
+
+[H, G] = myCostMatrices(Gamma,Phi,Q,R,P,N);
+
+zr22 = zeros(2, 2);
+zr33 = zeros(3, 3);
+Rbar = [R, zr22, zr22; zr22, R, zr22; zr22, zr22, R];
+Qbar = [Q, zr33, zr33; zr33, Q, zr33; zr33, zr33, P];
+H_actual = 2 * (Rbar + Gamma' * Qbar * Gamma);
+G_actual = 2 * Gamma' * Qbar * Phi;
+
+wrong2 = sum(sum(round(H, 8) ~= round(H_actual, 8))) + sum(sum(round(G, 8) ~= round(G_actual, 8)));
+disp(wrong2);
