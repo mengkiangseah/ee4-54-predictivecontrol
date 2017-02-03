33aff53e94bacb35a90944e8dfc8acfb83e608d3
Meng Kiang Seah <mengkiang92@gmail.com>
2017-02-03 21:55:24 +0000

    Changes to fulltest.m to fix output display.

--- assignment3/fulltest.m
+++ assignment3/fulltest.m
@@ -1,7 +1,7 @@
 % Testing script for Assignment 3 functions.
 
 % 0 to choose not to test. 1 to select.
-choices = [0, 0, 0, 0];
+choices = [1, 1, 0, 0];
 
 % Prepare some reused stuff.
 A = magic(3);
@@ -18,17 +18,19 @@ if choices(1) == 1
     [Dt,Et,bt] = myStageConstraints(A, B, D, cl, ch, ul, uh);
     
     Dt_correct = [D * A; -(D * A); zeros(4, 3)];
-    Et_correct = [D * B; -(D * B); I(2); I(2)];
+    Et_correct = [D * B; -(D * B); eye(2); -1 * eye(2)];
     bt_correct = [4; 4; 4; 4; -1; -1; -1; -1; 5; 5; -.5; -.5];
     
-    wrong1 = sum(sum(round(Dt_correct) ~= round(Dt))) + ...
-        sum(sum(round(Et_correct) ~= round(Et))) + ...
-        sum(sum(round(bt_correct) ~= round(bt)));
+    wrong1 = sum(sum(round(Dt_correct, 8) ~= round(Dt, 8))) + ...
+        sum(sum(round(Et_correct, 8) ~= round(Et, 8))) + ...
+        sum(sum(round(bt_correct, 8) ~= round(bt, 8)));
     
     if wrong1 > 0
         disp('Test for myStageConstraints.m failed.');
         disp('Wrong values: ');
         disp(wrong1);
+    else
+        disp('Test for myStageConstraints.m passed.');
     end
     
 end
@@ -37,7 +39,7 @@ end
 if choices(2) == 1    
     [Dt,Et,bt] = myStageConstraints(A, B, D, cl, ch, ul, uh);
     N = 3;
-    [DD,EE,bb] = myTrajectoryConstraints(Dt,Et,bt,N);
+    [DD,EE,bb] = myTrajectoryConstraints(Dt, Et, bt, N);
     
     zrd = zeros(12, 3);
     DD_correct = [Dt, zrd, zrd; zrd, Dt, zrd; zrd, zrd, Dt];
@@ -47,14 +49,16 @@ if choices(2) == 1
     
     bb_correct = [bt; bt; bt];
     
-    wrong2 = sum(sum(round(DD_correct) ~= round(DD))) + ...
-        sum(sum(round(EE_correct) ~= round(EE))) + ...
-        sum(sum(round(bb_correct) ~= round(bb)));
+    wrong2 = sum(sum(round(DD_correct, 8) ~= round(DD, 8))) + ...
+        sum(sum(round(EE_correct, 8) ~= round(EE, 8))) + ...
+        sum(sum(round(bb_correct, 8) ~= round(bb, 8)));
     
     if wrong2 > 0
         disp('Test for myTrajectoryConstraints.m failed.');
         disp('Wrong values: ');
         disp(wrong2);
+    else
+        disp('Test for myTrajectoryConstraints.m passed.');
     end
 end
 
