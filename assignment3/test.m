4eef867e8c6faa5345ffb4dc1cde8a90f11d3c28
Meng Kiang Seah <mengkiang92@gmail.com>
2017-02-03 21:07:15 +0000

    Testing script for the 4 files created, currently just a template.

---
+++ assignment3/test.m
@@ -0,0 +1,20 @@
+% Testing script for Assignment 3 functions.
+
+% 0 to choose not to test. 1 to select.
+choices = [0, 0, 0, 0];
+
+% Test for myStageConstraints.m
+if choices(1) == 1
+end
+
+% Test for myTrajectoryConstraints.m
+if choices(2) == 1
+end
+
+% Test for myConstraintMatrices.m
+if choices(3) == 1
+end
+
+% Test for myMPController.m
+if choices(4) == 1
+end
