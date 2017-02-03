e465fcb6785a5cdfc79a3b7bd1f793d74c9730c0
Meng Kiang Seah <mengkiang92@gmail.com>
2017-02-03 21:56:03 +0000

    myStageConstraints and myTrajectoryConstraints added, passing tests. Yet to check on Cody.

--- assignment3/myTrajectoryConstraints.m
+++ assignment3/myTrajectoryConstraints.m
@@ -1,6 +1,18 @@
-function [DD,EE,bb]=myTrajectoryConstraints(Dt,Et,bt,N)
+function [DD, EE, bb] = myTrajectoryConstraints(Dt, Et, bt, N)
+% MYTRAJECTORYCONSTRAINTS  [DD, EE, bb] = myTrajectoryConstraints(Dt, Et, bt, N)
+% DD is Dt expanded for all x
+% EE is Et expanded for all x
+% bb is bt expanded for all x
+% N is number of x
 
-% your code goes here
+    % DD is just Dt on a diagonal, sized N
+    DD = kron(eye(N), Dt);
+    
+    % EE is just Et on a diagonal, sized N
+    EE = kron(eye(N), Et);
+    
+    % bb is just bt stacked as many as N
+    bb =  kron(ones(N, 1), bt);
 
 end
 
