139f3f00950ef1794d1f8f471e8ab7d64644ac71
Meng Kiang Seah <mengkiang92@gmail.com>
2017-02-10 15:42:04 +0000

    Completed Task3, myConstraintMatrices.m

--- assignment3/myConstraintMatrices.m
+++ assignment3/myConstraintMatrices.m
@@ -1,6 +1,20 @@
-function [F,J,L]=myConstraintMatrices(DD,EE,Gamma,Phi,N)
-
-% your code goes here
-
+function [F, J, L] = myConstraintMatrices(DD, EE, Gamma, Phi, N)
+% MYCONSTRAINTMATRICES [F, J, L] = myConstraintMatrices(DD, EE, Gamma, Phi, N)
+% F, J, L are from the inequality equation. 
+% DD, EE, are from the expanded trajectory constraints.
+% Gamma and Phi are from the prediciton equation.
+% N is number of x
+    
+    % F is defined as Dbar x Gamma + Ebar
+    F = (DD * Gamma) + EE;
+    
+    % J is defined as - Dbar * Phi
+    J = -(DD * Phi);
+    
+    % L is defined as - J - (Dbar * (1N kron I))
+    % I needs to have columns of number of rows in x, i.e. n
+    % Phi is Nn * n, so n is columns in Phi
+    n = size(Phi, 2);
+    L = - J - (DD * kron(ones(N, 1), eye(n)));
 end
 
