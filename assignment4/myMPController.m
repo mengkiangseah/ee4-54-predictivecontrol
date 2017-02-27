17986e78e3e5ca878b9e7fddf48e59ab5a04ea31
Meng Kiang Seah <mengkiang92@gmail.com>
2017-02-27 15:37:52 +0000

    Files for Assignment 4 added into their own directory/

---
+++ assignment4/myMPController.m
@@ -0,0 +1,31 @@
+function [u,status,iA1] = myMPController(H,G,gs,F,bb,J,L,x,xTarget,nu,iA)
+% H       - quadratic term in the cost function (Linv if using mpcqpsolver).
+% G       - matrix in the linear term of the cost function.
+% gs      - vector in the linear term of the cost function.
+% F       - LHS of the inequalities term.
+% bb      - RHS of the inequalities term.
+% J       - RHS of inequalities term.
+% L       - RHS of inequalities term. Use this to shift constraints around target point
+% x       - current state
+% xTarget - target equilibrium point.
+% nu      - Number of inputs.
+% iA      - active inequalities, see doc mpcqpsolver
+%
+% u is the first input vector u_0 in the sequence [u_0; u_1; ...; u_{N-1}]; 
+% In other words, u is the value of the receding horizon control law
+% evaluated with the current state x and target xTarget
+
+% Please read the documentation on mpcqpsolver to understand how it is
+% suppose to be used. Use iA and iA1 to pass the active inequality vector 
+
+opt = mpcqpsolverOptions;
+%opt.IntegrityChecks = false; % for code generation
+opt.FeasibilityTol = 1e-3;
+opt.DataType = 'double';
+% your code starts here
+[U,status,iA1]=mpcqpsolver(%your code%,%your code%,%your code$,%your code%,[],zeros(0,1),iA,opt);
+% your code ends here
+% Copy and paste the indicated parts of your code above to the Simulink model
+u=U(1:nu);
+end
+
---
+++ assignment4/mySoftPadding.m
@@ -0,0 +1,9 @@
+function [Hs,gs,Fs,bs,Js,Ls] = mySoftPadding(H,F,bb,J,L,S,rho,m)
+% S = weight for quadratic cost of constraint violations
+% rho = scalar weight for 1-norm of constraint violations
+% m = number of inputs
+
+% your code here
+
+end
+
